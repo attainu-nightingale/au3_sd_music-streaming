dd15fa26849a82d41211a0f487bd9f5e30e59857
Roshan Kumar <roshankunaguero09@gmail.com>
2019-09-05 15:22:52 +0530

    Initial home page, side-nav, top-nav and all folder structure setup

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-var express=require("express")
-var app=express();
+var express=require("express");
+var hbs = require('hbs');
 
-app.get('/',function(req,res){
-    res.send("home");
-})
+var app = express();
+
+// load router
+var homeRouter = require('./routes/home');
+
+// hbs middleware
+app.set('view engine', 'hbs');
+
+// express body-parser middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// static folder middleware
+app.use(express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials/');
+
+
+hbs.registerHelper('is', function (parameter, string, options) {
+    if (parameter == string) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+// use router
+app.use('/', homeRouter);
+
+
+
+app.get('/library', function (req, res) {
+    res.render('library', {
+        title: 'Library'
+    });
+});
+
+
 
 
 app.listen(3000,function(req,res){
     console.log("listening at 3000");
 }
 );
+
+module.exports = app;
