14facc929134a4e0d026df8828ff4b40ab715b5a
Roshan Kumar <roshankunaguero09@gmail.com>
2019-09-06 19:09:20 +0530

    changed library to playlist

--- index.js
+++ index.js
@@ -33,14 +33,17 @@ app.use('/', homeRouter);
 
 
 
-app.get('/library', function (req, res) {
-    res.render('library', {
-        title: 'Library'
+app.get('/playlist', function (req, res) {
+    res.render('playlist', {
+        title: 'Playlist'
     });
 });
 
 
-
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function (req, res) {
+    res.send('<h1>what??? page not found!</h1>', 404);
+});
 
 app.listen(3000,function(req,res){
     console.log("listening at 3000");
