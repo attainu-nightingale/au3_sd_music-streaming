e301f54ade8dd0027de331787d966a40ce45a891
Bhaveshchowdary001@gmail.com <Bhaveshchowdary001@gmail.com>
2019-09-10 13:05:16 +0530

    temporary changes

--- public/layout.js
+++ public/layout.js
@@ -10,3 +10,43 @@
 $('#main').on('click', function() {
      $('#sidebar').removeClass('display');
 });
+
+$(".overlay").on('click',function(){
+    var y=event.target.getAttribute('data');
+    player=document.getElementById("audio0");
+    player.setAttribute('src',y);
+});
+
+$("#plus").on("click",function(e){
+    e.preventDefault();
+    var data={
+        image:$("#image").val(),
+        songName:$("#songName").val(),
+        audioSrc:$("#audioSrc").val()
+    }
+$.ajax({
+    type:"POST",
+    url:"/playlist/add",
+    data:data,
+    dataType:"json",
+    success:function(data){
+        console.log(data);
+    }
+});
+alert("song is added to playlist")    
+});
+
+
+$(".delete").on("click", function () {
+    var songId = $(this).attr("value");
+
+    $.ajax({
+        url: "/playlist/" + songId,
+        type: "delete",
+        dataType: "json",
+        success: function (data) {
+            alert("Deleted successfully");
+            location.reload()
+        }
+    });
+});
